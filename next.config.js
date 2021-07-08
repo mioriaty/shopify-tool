0690e3c6ba7882af4aa5fd24bb4c048a0bef4352
mioriaty <mioriaty@gmail.com>
2021-07-08 16:43:33 +0700

    test build 1

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const { parsed: localEnv } = require('dotenv').config();
 const webpack = require('webpack');
-
+const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(['wiloke-react-core']);
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 
-module.exports = withTM({
+module.exports = withPlugins([withTM], {
+  webpack5: false,
   webpack: config => {
     const env = { API_KEY: apiKey };
     config.plugins.push(new webpack.DefinePlugin(env));
-
     // Add ESM support for .mjs files in webpack 4
     config.module.rules.push({
       test: /\.mjs$/,
